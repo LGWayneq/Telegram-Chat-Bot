d12c59a7f203b0d8baa40c9e87138e2be099db3e
LGWayneq <67624713+LGWayneq@users.noreply.github.com>
2021-09-21 15:05:49 +0800

    Update stickerdata.py

--- stickerdata.py
+++ stickerdata.py
@@ -1,17 +1,14 @@
 from random import randint
 
-pingupatid = "CAACAgQAAxkBAAIB1GCo4Jza4yqdLt_1JkLNvTHNwWz-AAInAQACRWLRAYKKLxJy0UliHwQ"
-pingucuddleid = "CAACAgQAAxkBAAIB2mCo5004cUM6ql96rAriL2tCZgnqAAIsAQACRWLRAZ29uBQSbSpJHwQ"
-pinguhugid = "CAACAgQAAxkBAAIB22Co53sD3JWIrnMN7v4MvpnVl4Z9AAITAQACRWLRAZCAooz1PvdXHwQ"
-pinguplayid = "CAACAgQAAxkBAAIB3GCo56n4IlFOMvlZFrcW9fD83T00AAIyAQACRWLRAQ88uDQPKZxzHwQ"
-pinguhappyid = "CAACAgQAAxkBAAIB3WCo57__qceZCTC-UoRCrz6p9DgfAAIcAQACRWLRAXLdhJSCB_D4HwQ"  
-pingulist = [pingupatid, pingucuddleid, pinguhugid, pinguplayid, pinguhappyid]
+penguinpatid = "CAACAgQAAxkBAAIB1GCo4Jza4yqdLt_1JkLNvTHNwWz-AAInAQACRWLRAYKKLxJy0UliHwQ"
+penguincuddleid = "CAACAgQAAxkBAAIB2mCo5004cUM6ql96rAriL2tCZgnqAAIsAQACRWLRAZ29uBQSbSpJHwQ"
+penguinhugid = "CAACAgQAAxkBAAIB22Co53sD3JWIrnMN7v4MvpnVl4Z9AAITAQACRWLRAZCAooz1PvdXHwQ"
+penguinplayid = "CAACAgQAAxkBAAIB3GCo56n4IlFOMvlZFrcW9fD83T00AAIyAQACRWLRAQ88uDQPKZxzHwQ"
+penguinhappyid = "CAACAgQAAxkBAAIB3WCo57__qceZCTC-UoRCrz6p9DgfAAIcAQACRWLRAXLdhJSCB_D4HwQ"  
+penguinlist = [pingupatid, pingucuddleid, pinguhugid, pinguplayid, pinguhappyid]
 
-buttshakeid = "CAACAgIAAxkBAAIB2GCo4murBHqFOwW1i_Jlx1zf1iJFAAJxEgAC6NbiEpNossZGq9nyHwQ"
 fooddanceid = "CAACAgIAAxkBAAIB3mCpJFrCiTt4DzLG4wyYdTK8uXY_AAJcEgAC6NbiEr5Jo5N45fhsHwQ"
 
-def randompinguid():
-    global pingulist
-    return pingulist[randint(0,len(pingulist)-1)]
-
-print(randompinguid())
+def randomstickerid():
+    global penguinlist
+    return penguinlist[randint(0,len(penguinlist)-1)]
